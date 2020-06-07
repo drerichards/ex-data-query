a0888884dab15f0a30789c6d75dde9b7a368be45
Gerard Richardson <gerardr@spotify.com>
2020-06-07 03:36:08 -0400

    add users and posts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,52 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+const users = [
+    {
+        id: '1',
+        name: 'Andre',
+        email: 'Andre@email.com',
+        age: 32
+    },
+    {
+        id: '2',
+        name: 'Mark',
+        email: 'Mark@email.com',
+        age: 36
+    },
+    {
+        id: '3',
+        name: 'Rich',
+        email: 'Rich@email.com',
+        age: 34
+    },
+]
+
+const posts = [
+    {
+        id: '001',
+        title: '1 Title',
+        body: 'Content...',
+        published: true
+    },
+    {
+        id: '002',
+        title: '2 Title',
+        body: 'Content...',
+        published: false
+    },
+    {
+        id: '003',
+        title: '3 Title',
+        body: 'Content...',
+        published: true
+    },
+]
+
+
 const typeDefs = `
     type Query {
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
         me: User!
         post: Post!
     }
@@ -23,6 +68,22 @@ const typeDefs = `
 
 const resolvers = {
     Query: {
+        users(parent, args, ctx) {
+            if (!args.query) {
+                return users
+            }
+            return users.filter(user => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        posts(parent, args, ctx) {
+            if (!args.query) {
+                return posts
+            }
+            return posts.filter(post => {
+                return post.title.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
         me() {
             return {
                 id: '7984621',
